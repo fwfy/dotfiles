533a45cb54113bc01ce9ed1dc1ae11613fa6185f
Aedan Ball <fwfy+codesign@fwfy.club>
2025-06-29 00:25:51 -0400

    feat: add updater

---
+++ updater.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Pulling latest changes...
+git pull
+echo Executing stow...
+for d in */ ; do
+	echo "Updating $d"
+	# stow $d
+done
+echo Updates finished. You should probably reload.
